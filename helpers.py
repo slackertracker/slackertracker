1c99c69a3710e7f1ee57151c0a7ecb9959fd1d8e
Nathan Perkins <nathan.perkins@ubisoft.com>
2018-03-30 17:01:24 -0700

    jsonify the responses, add_to_slack button, handle challenge on various apis

--- helpers.py
+++ helpers.py
@@ -1,5 +1,7 @@
 from flask import jsonify, Response
 
-def get_challenge_response(data):
-    return jsonify({ "challenge": data["challenge"] })
-    
+def get_challenge_response(data, valid_token):
+    if data['token'] == valid_token:
+        return jsonify({ "challenge": data["challenge"] })
+    else:
+        return("Invalid token.")
