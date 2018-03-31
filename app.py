9ba290d7402f3b2fc265a2dd9f7d3abf6a7f1cb7
Nathan Perkins <nathan.perkins@ubisoft.com>
2018-03-30 23:10:19 -0700

    add incoming event handler

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from slackertracker.helpers import get_challenge_response 
+# helpers
+from slackertracker.helpers import get_challenge_response
+from slackertracker.helpers import get_user_by_slack_id
+from slackertracker.helpers import get_channel_by_slack_id
 
 # Stuff for routing
 from flask import request
@@ -132,21 +135,64 @@ def create_app(config_file):
         elif data.get('type') == 'event_callback':
             event = data.get('event')
             item = event.get('item')
-            channel_id = ''
-            sender_id = event.get('user')
-            receiver_id = event.get('item_user')
-            name = event.get('reaction')
+            team_id = data.get('team_id')
 
+            sender_id = event.get('user')
+            sender = User.query.filter_by(slack_id=sender_id).first()
+            if sender is None:
+                user_data = get_user_by_slack_id(sender_id)
+
+                sender = User(
+                    display_name = user_data.get('display_name'),
+                    slack_id = sender_id,
+                    team_id = team_id
+                )
+
+                print(sender.slack_id + sender.display_name + sender.team_id)
+                db.session.add(sender)
+                db.session.commit()
+                sender = User.query.filter_by(slack_id=sender_id).first()
+
+            receiver_id = event.get('item_user')
+            receiver = User.query.filter_by(slack_id=receiver_id).first()
+            if receiver is None:
+                user_data = get_user_by_slack_id(receiver_id)
+                receiver = User(
+                    slack_id = receiver_id,
+                    display_name = user_data.get('display_name'),
+                    team_id = team_id
+                )
+                db.session.add(receiver)
+                db.session.commit()
+                receiver = User.query.filter_by(slack_id=receiver_id).first()
+            
             if item.get('type') == 'message':
                 channel_id = item.get('channel')
+                channel = Channel.query.filter_by(slack_id=channel_id).first()
 
-            reaction = {
-                'name': name,
-                'sender_id': sender_id,
-                'receiver_id': receiver_id,
-                'channel_id': channel_id,
-            }
+                if not channel:
+                    channel_data = get_channel_by_slack_id(channel_id)
+                    channel = Channel(
+                        slack_id = channel_id,
+                        team_id = team_id,
+                        name = channel_data.get('name'),
+                        is_private = channel_data.get('is_private')
+                    )
+                    db.session.add(channel)
+                    db.session.commit()
+                    channel = Channel.query.filter_by(slack_id=channel_id).first()
 
-            return(jsonify(reaction))
+            reaction = Reaction(
+                name = event.get('reaction'),
+                team_id = team_id,
+                sender_id = sender_id,
+                receiver_id = receiver_id,
+                channel_id = channel_id
+            )
+
+            db.session.add(reaction)
+            db.session.commit()
+
+            return(jsonify(reaction.serialize()))
 
     return(app)
--- helpers.py
+++ helpers.py
@@ -17,10 +17,33 @@ def get_user_by_slack_id(slack_id):
     }
 
     user = requests.get(url + slack_id, headers=headers).json().get('user')
+    if user is None:
+        return({})
+        
     profile = user.get('profile')
 
     return({
         'display_name': profile.get('display_name_normalized'),
         'slack_id': slack_id,
         'team_id': user.get('team_id')
+    })
+
+def get_channel_by_slack_id(slack_id):
+    from slackertracker.instance.config import SLACK_LEGACY_TOKEN
+
+    url = "https://slack.com/api/channels.info?channel="
+
+    headers = { 
+        "Authorization": "Bearer " + SLACK_LEGACY_TOKEN,
+    }
+
+    channel = requests.get(url + slack_id, headers=headers).json().get('channel')
+
+    if not channel:
+        return({})
+
+    return({
+        'slack_id': slack_id,
+        'name': channel.get('name'),
+        'is_private': channel.get('is_private')
     })
