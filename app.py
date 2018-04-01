75719efb92224074fe40106b252b65845c481a8d
Sonam Kindy <kindys@oregonstate.edu>
2018-04-01 21:47:18 +0000

    Plugged in linking username re: discussion on slack

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ def create_app(config_file):
                 for reaction in user.reactions_received:
                     reaction_counts[reaction.name] = reaction_counts.get(reaction.name, 0) + 1
 
-            resp_pretext = "*Top 5 emoji reactions received by {}* (_ahem, you!_)".format(user_name)
+            resp_pretext = "*Top 5 emoji reactions received by <@{}>* (_ahem, you!_)".format(slack_user_id)
 
             sorted_reactions = sorted(reaction_counts, key=reaction_counts.get, reverse=True)
             resp_text = 'Count is displayed in parens. :nerd_face:\n\n'
@@ -198,6 +198,7 @@ def create_app(config_file):
         slash_command = data.get('command')
         
         resp_text = ("To see your karma score: {0}\n"
+            
                     "To see another user's karma score: {0} @username\n"
                     "To see a channel's top 5 most-used emojis: {0} #channel\n"
                     "To see a list of commands you can use (what you're seeing now): {0} help"
